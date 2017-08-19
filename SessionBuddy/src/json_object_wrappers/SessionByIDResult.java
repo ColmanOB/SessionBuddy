7cce86a006cf571f2b9c7b5ed805ee98293c0882
Colman <Colman@HomeLaptop>
2017-08-19 11:40:51 +0100

    Began work on 'retrieve session by ID' functionality

---
+++ SessionBuddy/src/json_object_wrappers/SessionByIDResult.java
@@ -0,0 +1,56 @@
+package json_object_wrappers;
+
+import java.util.ArrayList;
+
+// TODO: Adapt this from Tune to Session... No refactoring done yet
+
+/**
+ * @author Colman
+ * @since 2017-08-17
+ */
+public class SessionByIDResult 
+	{
+	// Variables to hold data relating to the tune as a whole
+	public String id;	
+	public String name;
+	public String url;
+	
+	public User member;
+	
+	public String date;
+	public String type;
+	public String tunebooks;
+	public String recordings;
+	
+	public ArrayList<String> aliases;
+	
+	public ArrayList<TuneSetting> settings; // An array of different settings of the particular tune
+
+	/**
+	 * Constructor method
+	 * 
+	 * @param id
+	 * @param name
+	 * @param url
+	 * @param member
+	 * @param date
+	 * @param type
+	 * @param tunebooks
+	 * @param recordings
+	 * @param aliases
+	 * @param settings
+	 */
+	public SessionByIDResult(String id, String name, String url, User member, String date, String type, String tunebooks, String recordings, ArrayList<String> aliases, ArrayList<TuneSetting> settings)
+		{
+		this.id = id;
+		this.name = name;
+		this.url = url;	
+		this.member = member;
+		this.date = date;
+		this.type = type;
+		this.tunebooks = tunebooks;
+		this.recordings = recordings;
+		this.aliases = aliases;
+		this.settings = settings;
+		}
+}
