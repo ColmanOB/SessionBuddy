c7208eeec17f68cf6b897a97b70ef049d23b1964
Colman <Colman@HomeLaptop>
2017-08-27 14:32:19 +0100

    Major refactoring of SearchByLocation methods

---
+++ SessionBuddy/src/example_usage/Test_SearchByLocation_Events.java
@@ -0,0 +1,49 @@
+package example_usage;
+
+import java.util.ArrayList;
+
+import json_object_wrappers.EventsByLocationResult;
+import main.SearchByLocation;
+
+
+public class Test_SearchByLocation_Events
+{
+public static void main(String[] args)
+   {
+	// Set the search parameters
+	String latitude= "53.3498";
+	String longitude = "6.2603";
+	String radius = "500";
+	int resultsPerPage = 50;
+	
+	// Instantiate a TheSessionAPISearcher object
+	SearchByLocation search = new SearchByLocation();
+	
+	// Pass in the search parameters
+	ArrayList<EventsByLocationResult> resultSet = search.searchEventsByLocation(latitude, longitude, radius, resultsPerPage);
+	
+	// Loop through the results and print each attribute of each individual result in the set
+	for (int i = 0; i < resultSet.size(); i++)
+		{
+		System.out.println(resultSet.get(i).details.eventID);
+		System.out.println(resultSet.get(i).details.eventName);
+		System.out.println(resultSet.get(i).details.eventURL);	
+				
+		System.out.println(resultSet.get(i).user.userID);
+		System.out.println(resultSet.get(i).user.userName);
+		System.out.println(resultSet.get(i).user.userURL);
+		
+		System.out.println(resultSet.get(i).venue.venueID);
+		System.out.println(resultSet.get(i).venue.venueName);
+		System.out.println(resultSet.get(i).venue.venueEmail);
+		System.out.println(resultSet.get(i).venue.venuePhone);
+		
+		System.out.println(resultSet.get(i).town.townName);
+		
+		System.out.println(resultSet.get(i).country.countryName);
+		
+		System.out.println("\n");
+		}
+   	}
+
+}
--- SessionBuddy/src/main/SearchByLocation.java
+++ SessionBuddy/src/main/SearchByLocation.java
@@ -7,18 +7,20 @@ import org.apache.commons.lang3.StringEscapeUtils;
 import json_object_wrappers.Area;
 import json_object_wrappers.Coordinates;
 import json_object_wrappers.Country;
+import json_object_wrappers.EventDetails;
+import json_object_wrappers.EventSchedule;
+import json_object_wrappers.EventsByLocationResult;
 import json_object_wrappers.SessionDetails;
 import json_object_wrappers.SessionsByLocationResult;
 import json_object_wrappers.Town;
 import json_object_wrappers.User;
 import json_object_wrappers.Venue;
+import response_parsers.EventsByLocationParser;
 import response_parsers.SessionsByLocationParser;
+import result_set_wrappers.EventsByLocationWrapper;
 import result_set_wrappers.SessionsByLocationWrapper;
 
 
-// TODO: Create a method to search for events by location
-// TODO: Create a method to search for events by location, including the capability to specify a page number
-// TODO: Create a helper method to handle the parsing of the event by location search results
 // TODO: Complete the Javadoc comments for each of the methods
 
 /**
@@ -33,6 +35,8 @@ public class SearchByLocation
 	private int pageCount = 0;
 	
 	/**
+	 * Queries the API for a list of sessions within a specified radius of a particular latitude and longitude
+	 * 
 	 * @param latitude
 	 * @param longitude
 	 * @param radius
@@ -42,9 +46,16 @@ public class SearchByLocation
 	 */
 	public ArrayList<SessionsByLocationResult> searchSessionsByLocation(String latitude, String longitude, String radius, int resultsPerPage) throws IllegalArgumentException
 		{
-		if (resultsPerPage > 50)
+		try
 			{
-			throw new IllegalArgumentException("Number of results per page must be 50 or less");
+			// Validate the user input
+			validateResultsPerPageCount(resultsPerPage);
+			validateCoordinates(latitude, longitude, radius);
+			}
+		
+		catch (IllegalArgumentException e)
+			{
+			throw new IllegalArgumentException(e.getMessage());
 			}
 	
 		// Launch a search for a list of matching recordings and store the JSON that is returned as a String
@@ -64,7 +75,7 @@ public class SearchByLocation
 		}
 	
 	
-	/**
+	/** 
 	 * Alternative version of searchSessionsByLocation(), allowing a page number to be specified within a paginated JSON response.
 	 * 
 	 * @param latitude
@@ -76,11 +87,18 @@ public class SearchByLocation
 	 */
 	public ArrayList<SessionsByLocationResult> searchSessionsByLocation(String latitude, String longitude, String radius, int resultsPerPage, int pageNumber) throws IllegalArgumentException
 		{
-		if (resultsPerPage > 50)
+		try
 			{
-			throw new IllegalArgumentException("Number of results per page must be 50 or less");
+			// Validate the user input
+			validateResultsPerPageCount(resultsPerPage);
+			validateCoordinates(latitude, longitude, radius);
 			}
-	
+		
+		catch (IllegalArgumentException e)
+			{
+			throw new IllegalArgumentException(e.getMessage());
+			}
+		
 		// Launch a search for a list of matching recordings and store the JSON that is returned as a String
 		HttpRequestor searcher = new HttpRequestor();
 		String apiQueryResults = searcher.submitLocationRequest("sessions", latitude, longitude, radius, resultsPerPage, pageNumber);
@@ -93,6 +111,89 @@ public class SearchByLocation
 		ArrayList<SessionsByLocationResult> resultSet = new ArrayList <SessionsByLocationResult>();
 		
 		resultSet = populateSessionsByLocationResult(parsedResults);
+		
+		return resultSet;
+		}
+	
+	
+	/**
+	 * Queries the API for a list of sessions within a specified radius of a particular latitude and longitude
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param radius
+	 * @param resultsPerPage
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public ArrayList<EventsByLocationResult> searchEventsByLocation(String latitude, String longitude, String radius, int resultsPerPage) throws IllegalArgumentException
+		{
+		try
+			{
+			// Validate the user input
+			validateResultsPerPageCount(resultsPerPage);
+			validateCoordinates(latitude, longitude, radius);
+			}
+	
+		catch (IllegalArgumentException e)
+			{
+			throw new IllegalArgumentException(e.getMessage());
+			}
+	
+		// Launch a search for a list of matching events and store the JSON that is returned as a String
+		HttpRequestor searcher = new HttpRequestor();
+		String apiQueryResults = searcher.submitLocationRequest("events", latitude, longitude, radius, resultsPerPage);
+			
+		// Parse the returned JSON into a wrapper class to allow access to all elements
+		EventsByLocationParser jsonParser = new EventsByLocationParser();
+		EventsByLocationWrapper parsedResults = jsonParser.parseResponse(apiQueryResults);
+			
+		// This will hold each individual search result entry
+		ArrayList<EventsByLocationResult> resultSet = new ArrayList <EventsByLocationResult>();
+		
+		resultSet = populateEventsByLocationResult(parsedResults);
+		
+		return resultSet;
+		}
+	
+	
+	/**
+	 * Alternative version of searchEventsByLocation, allowing an individual page to be specified within a paginated JSON response
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param radius
+	 * @param resultsPerPage
+	 * @param pageNumber
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	public ArrayList<EventsByLocationResult> searchEventsByLocation(String latitude, String longitude, String radius, int resultsPerPage, int pageNumber) throws IllegalArgumentException
+		{
+		try
+			{
+			// Validate the user input
+			validateResultsPerPageCount(resultsPerPage);
+			validateCoordinates(latitude, longitude, radius);
+			}
+	
+		catch (IllegalArgumentException e)
+			{
+			throw new IllegalArgumentException(e.getMessage());
+			}
+	
+		// Launch a search for a list of matching events and store the JSON that is returned as a String
+		HttpRequestor searcher = new HttpRequestor();
+		String apiQueryResults = searcher.submitLocationRequest("events", latitude, longitude, radius, resultsPerPage, pageNumber);
+			
+		// Parse the returned JSON into a wrapper class to allow access to all elements
+		EventsByLocationParser jsonParser = new EventsByLocationParser();
+		EventsByLocationWrapper parsedResults = jsonParser.parseResponse(apiQueryResults);
+			
+		// This will hold each individual search result entry
+		ArrayList<EventsByLocationResult> resultSet = new ArrayList <EventsByLocationResult>();
+		
+		resultSet = populateEventsByLocationResult(parsedResults);
 		
 		return resultSet;
 		}
@@ -136,9 +237,51 @@ public class SearchByLocation
 		// Return the set of results that has been collected
 		return resultSet;
 		}
+
+	
+	/**
+	 * Helper method to gather and parse the response to a location-based search for sessions
+	 * 
+	 * @param parsedResults
+	 * @return
+	 */
+	private ArrayList<EventsByLocationResult> populateEventsByLocationResult(EventsByLocationWrapper parsedResults)	
+		{	
+		// This will hold each individual search result entry
+		ArrayList<EventsByLocationResult> resultSet = new ArrayList <EventsByLocationResult>();
+		
+		//Find out how many pages are in the response, to facilitate looping through multiple pages
+		pageCount = Integer.parseInt(parsedResults.pages);
+			
+		// Loop as many times as the count of events in the result set:
+		for(int i = 0; i < (parsedResults.events.length)-1; i++)
+			{
+			// Extract the required elements from each individual search result in the JSON response
+			// StringEscapeUtils.unescapeXml() will decode the &039; etc. XML entities from the JSON response
+			EventDetails details = new EventDetails(parsedResults.events[i].id, StringEscapeUtils.unescapeXml(parsedResults.events[i].name), parsedResults.events[i].url, parsedResults.events[i].date);
+			EventSchedule schedule = new EventSchedule(parsedResults.events[i].dtstart, parsedResults.events[i].dtend);
+			Coordinates coordinates = new Coordinates(parsedResults.events[i].latitude, parsedResults.events[i].longitude);
+			User user = new User(Integer.toString(parsedResults.events[i].member.id),StringEscapeUtils.unescapeXml(parsedResults.events[i].member.name),parsedResults.events[i].member.url);
+			Venue venue = new Venue(Integer.toString(parsedResults.events[i].venue.id), StringEscapeUtils.unescapeXml(parsedResults.events[i].venue.name), parsedResults.events[i].venue.telephone, parsedResults.events[i].venue.email, parsedResults.events[i].venue.web );
+			Town town = new Town(Integer.toString(parsedResults.events[i].town.id), StringEscapeUtils.unescapeXml(parsedResults.events[i].town.name));
+			Area area = new Area(Integer.toString(parsedResults.events[i].area.id), StringEscapeUtils.unescapeXml(parsedResults.events[i].area.name));
+			Country country = new Country(Integer.toString(parsedResults.events[i].country.id), StringEscapeUtils.unescapeXml(parsedResults.events[i].country.name));
+			
+			// Instantiate a EventsByLocationResult object & populate it
+			EventsByLocationResult currentResult = new EventsByLocationResult(details, user, schedule, coordinates, venue, town, area, country);
+			
+			// Add the EventsByLocationResult object to the ArrayList to be returned to the caller
+			resultSet.add(currentResult);
+			}
+		
+		// Return the set of results that has been collected
+		return resultSet;
+		}
 	
 	
 	/**
+	 * Helper method to keep track of the number of pages in the JSON response form the API
+	 * 
 	 * @return
 	 * @throws IllegalStateException
 	 */
@@ -151,4 +294,75 @@ public class SearchByLocation
 		else 
 			return pageCount;
 		}
+	
+	
+	/**
+	 * Helper method to validate that the user has specified a value between 1-50 for the results per page
+	 * 
+	 * @param resultsPerPage
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	private boolean validateResultsPerPageCount(int resultsPerPage) throws IllegalArgumentException
+		{
+		if (resultsPerPage <= 0)
+			{
+			// Specifying zero or a negative number of results per page should not be allowed
+			throw new IllegalArgumentException("Number of results per page must be greater than zero");
+			}
+		
+		if (resultsPerPage > 50)
+			{
+			// The API only allows a maximum of 50 results per page
+			throw new IllegalArgumentException("Number of results per page cannot exceed 50");
+			}
+		
+		// The value specified is in the range 1 - 50 and is valid
+		else return true;
+		}
+	
+	
+	/**
+	 * Helper method to validate the latitude, longitude and radius provided by the user
+	 * 
+	 * @param latitude
+	 * @param longitude
+	 * @param radius
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	private boolean validateCoordinates(String latitude, String longitude, String radius) throws IllegalArgumentException
+		{	
+		// Specify a set of rules defining valid coordinates
+		final float MIN_LATITUDE = Float.valueOf("-90.0000");
+		final float MAX_LATITUDE = Float.valueOf("90.0000");
+		final float MIN_LONGITUDE = Float.valueOf("-180.0000");
+		final float MAX_LONGITUDE = Float.valueOf("180.0000");
+		
+		// I chose to allow a radius of up to 1000Km - assuming nobody is likely to want to search within a larger radius
+		final float MIN_RADIUS = Float.valueOf("1.0000");
+		final float MAX_RADIUS = Float.valueOf("1000.0000");
+		
+		// First check if an invalid latitude value was provided
+		if (Float.valueOf(latitude) < MIN_LATITUDE || Float.valueOf(latitude) > MAX_LATITUDE) 
+			{
+			throw new IllegalArgumentException("Invalid latitude value - must be between -90 and 90");
+			}
+		
+		// Next, check if an invalid longitude value was provided
+		if (Float.valueOf(longitude) < MIN_LONGITUDE || Float.valueOf(longitude) > MAX_LONGITUDE) 
+			{
+			throw new IllegalArgumentException("Invalid longitude value - must be between -180 and 180");
+			}
+		
+		// Finally, check if a radius of less than zero or more than 1000 Km was provided
+		if (Float.valueOf(radius) < MIN_RADIUS || Float.valueOf(radius) > MAX_RADIUS) 
+			{
+			throw new IllegalArgumentException("Invalid radius value - must be between 1 and 1000");
+			}
+		
+		// Otherwise conclude that the input coordinates are valid
+		else return true;	
+		}
+	
 }
