b73c36f29542d1d70f86567954bc23942c80ffca
Colman <Colman@HomeLaptop>
2017-09-23 12:22:29 +0100

    Fixed cockup with gitignore that was created in error

---
+++ SessionBuddy/src/example_usage/Test_KeywordSearch_Discussions_with_Pagination.java
@@ -0,0 +1,65 @@
+package example_usage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import sessionbuddy.KeywordSearch;
+import sessionbuddy.wrappers.resultsets.SearchResultsDiscussions;
+
+/**
+ * Example usage of the searchDiscussions method of KeywordSearch class to search thesession.org API for a discussion based on a set of search terms, and store the results.
+ * 
+ * This class does not test the iteration through multiple pages of JSON search results
+ * 
+ * @author Colman
+ * @since 2017-08-13
+ */
+
+public class Test_KeywordSearch_Discussions_with_Pagination
+	{
+	
+	public static void main(String[] args)
+	   {
+		// Set the search parameters
+		String searchTerms = "Humours";
+		int resultsPerPage = 2;
+		int pageNumber = 1;
+		
+		// Create a structure to hold the data from the response
+		ArrayList<SearchResultsDiscussions> resultSet;
+		
+		// Instantiate a KeywordSearch object
+		KeywordSearch search = new KeywordSearch();
+		
+		try
+			{
+			// Pass in the search parameters
+			resultSet = search.searchDiscussions(searchTerms, resultsPerPage, pageNumber);
+			
+			// Loop through the results and print each attribute of each individual result in the set
+			for (int i = 0; i < resultSet.size(); i++)
+				{
+				System.out.println("Discussion ID: " + resultSet.get(i).details.discussionID);
+				System.out.println("Discussion Title: " + resultSet.get(i).details.discussionName);
+				System.out.println("Date Submitted: " + resultSet.get(i).details.submittedDate);
+				System.out.println("Discussion URL: " + resultSet.get(i).details.discussionURL);	
+				System.out.println("No. of comments: " + resultSet.get(i).details.numberOfComments);	
+						
+				System.out.println("Submitter User ID: " + resultSet.get(i).user.userID);
+				System.out.println("Submitter Username: " + resultSet.get(i).user.userName);
+				System.out.println("Submitter Profile Page: " + resultSet.get(i).user.userURL);
+				System.out.println("\n");
+				}
+			}
+		
+		catch (IllegalArgumentException e)
+			{
+			System.out.println(e.getMessage());
+			}	
+		
+		catch (IOException e)
+			{
+			System.out.println(e.getMessage());
+			}
+	   	}
+	}
---
+++ SessionBuddy/src/example_usage/Test_KeywordSearch_Events_with_Pagination.java
@@ -0,0 +1,81 @@
+package example_usage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import sessionbuddy.KeywordSearch;
+import sessionbuddy.wrappers.resultsets.SearchResultEvents;
+
+public class Test_KeywordSearch_Events_with_Pagination
+{
+public static void main(String[] args)
+   {
+	// Set the search parameters
+	String searchTerms = "Dublin";
+	int resultsPerPage = 2;
+	int pageNumber = 2;
+	
+	// Instantiate a KeywordSearch object
+	KeywordSearch search = new KeywordSearch();
+	
+	// Create a structure to hold the response from the API
+	ArrayList<SearchResultEvents> resultSet;
+	
+	try 
+		{
+		// Call the searchEvents method on the KeywordSearch object
+		resultSet = search.searchEvents(searchTerms, resultsPerPage, pageNumber);
+		
+		// Loop through the results and print each attribute of each individual result in the set
+		for (int i = 0; i < resultSet.size(); i++)
+			{
+			System.out.println("Event ID: " + resultSet.get(i).details.eventID);
+			System.out.println("Event Name: " + resultSet.get(i).details.eventName);
+			System.out.println("Date Submitted: " + resultSet.get(i).details.submittedDate);
+			System.out.println("Event URL: " + resultSet.get(i).details.eventURL);	
+			
+			System.out.println("Venue ID: " + resultSet.get(i).venue.venueID);
+			System.out.println("Venue Name: " + resultSet.get(i).venue.venueName);
+			System.out.println("Venue Email: " + resultSet.get(i).venue.venueEmail);
+			System.out.println("Venue Phone Number: " + resultSet.get(i).venue.venuePhone);
+			System.out.println("Venue Website: " + resultSet.get(i).venue.venueWebsite);
+			
+			System.out.println("Town/City ID: " + resultSet.get(i).town.townID);
+			System.out.println("Town/City Name: " + resultSet.get(i).town.townName);
+			
+			System.out.println("Area ID: " + resultSet.get(i).area.areaID);
+			System.out.println("Area Name: " + resultSet.get(i).area.areaName);
+			
+			System.out.println("Country ID: " + resultSet.get(i).country.countryID);
+			System.out.println("Country Name: " + resultSet.get(i).country.countryName);
+			
+			System.out.println("Latitude: " + resultSet.get(i).coordinates.latitude);
+			System.out.println("Longitude: " + resultSet.get(i).coordinates.longitude);
+			
+			System.out.println("Event Start Date: " + resultSet.get(i).schedule.startDate);
+			System.out.println("Event End Date: " + resultSet.get(i).schedule.endDate);
+			
+			System.out.println("Submitter User ID: " + resultSet.get(i).user.userID);
+			System.out.println("Submitter Username: " +resultSet.get(i).user.userName);
+			System.out.println("Submitter Profile Page: " +resultSet.get(i).user.userURL);
+			
+			System.out.println("\n");
+			}
+		} 
+	
+	catch (IllegalArgumentException e) 
+		{
+		e.printStackTrace();
+		} 
+	
+	catch (IllegalStateException e) 
+		{
+		e.printStackTrace();
+		} 
+	
+	catch (IOException e) 
+		{
+		e.printStackTrace();
+		}
+   	}
+}
