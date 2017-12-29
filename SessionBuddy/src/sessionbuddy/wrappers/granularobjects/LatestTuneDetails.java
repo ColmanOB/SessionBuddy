9b78fa8702ddaabecead99c36c620042d71b458f
Colman <Colman@HomeLaptop>
2017-12-29 08:37:00 +0000

    Continued review of JavaDoc comments

--- SessionBuddy/src/sessionbuddy/wrappers/granularobjects/LatestTuneDetails.java
+++ SessionBuddy/src/sessionbuddy/wrappers/granularobjects/LatestTuneDetails.java
@@ -4,15 +4,33 @@ package sessionbuddy.wrappers.granularobjects;
  * Represents the tune to which a setting belongs, in the result set when retrieving the list of latest tunes (i.e. latest settings)
  * 
  * @author Colman O'B
- * @since 2017-09-10
+ * @since 2017-12-29
  */
 public class LatestTuneDetails 
 	{
+	/**
+	 * A numeric ID for the tune in thesession.org database
+	 */
 	public String tuneID;
+	
+	/**
+	 * The name of the tune in thesession.org database
+	 */
 	public String tuneName;
+	
+	/**
+	 * The URL of the tune's page on thesession.org website
+	 */
 	public String tuneURL;
 
 
+	/**
+	 * Constructor that populates all fields
+	 * 
+	 * @param tuneID A numeric ID for the tune in thesession.org database
+	 * @param tuneName The name of the tune in thesession.org database
+	 * @param tuneURL The URL of the tune's page on thesession.org website
+	 */
 	public LatestTuneDetails(String tuneID, String tuneName, String tuneURL)
 		{
 		this.tuneID = tuneID;
