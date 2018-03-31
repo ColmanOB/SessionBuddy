fe0e5301e2a2a7cdf92c5e85253adafda2f09b2a
Colman <Colman@HomeLaptop>
2018-03-31 01:20:54 +0100

    Created first unit test for UrlBuilderTest

--- SessionBuddy/test/utils/UrlBuilderTest.java
+++ SessionBuddy/test/utils/UrlBuilderTest.java
@@ -2,15 +2,39 @@ package utils;
 
 import static org.junit.Assert.*;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.junit.Test;
+
+import sessionbuddy.utils.UrlBuilder;
 
 public class UrlBuilderTest 
 	{
 
 	@Test
-	public void test() 
+	public void testKeywordSearchURLWithPagination() 
 		{
-		fail("Not yet implemented");
+		UrlBuilder builder = new UrlBuilder();
+		
+		String searchTerms = "wig glue";
+		int resultsPerPage = 5;
+		int pageNumber = 2;
+		List<NameValuePair> queryParams = new ArrayList<>();
+		queryParams.add(new BasicNameValuePair("q", searchTerms));
+		
+		URL requestURL = builder.new Builder()
+				.path("discussions" + "/" + "search")
+				.queryParameters(queryParams)
+				.itemsPerPage(resultsPerPage)
+				.pageNumber(pageNumber)
+				.build();
+		
+		// Test that the sequence "&amp;" is replaced by a single ampersand
+		assertEquals("https://thesession.org/discussions/search?q=wig+glue&format=json&perpage=5&page=2", requestURL.toString());
 		}
 
 	}
