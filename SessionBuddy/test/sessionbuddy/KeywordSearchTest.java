028f8bd002645b14c3c8d280b2f54e1c175506b8
Colman <Colman@HomeLaptop>
2018-04-03 23:34:14 +0100

    Minor changes to unit test

--- SessionBuddy/test/sessionbuddy/KeywordSearchTest.java
+++ SessionBuddy/test/sessionbuddy/KeywordSearchTest.java
@@ -91,4 +91,43 @@ public class KeywordSearchTest
 			fail(e.getMessage());
 			}	
 		}
+	
+	@Test
+	public void testSearchEventssWithoutPagination() 
+		{
+		// TODO: change this to search for events rather than discussions
+		// Set the search parameters
+		String searchTerms = "Wig Glue";
+		int resultsPerPage = 10;
+	
+		try
+			{
+			// Instantiate a KeywordSearch object
+			KeywordSearch search = new KeywordSearch(searchTerms, resultsPerPage);
+			
+			// Call the KeywordSearch.searchDiscussions() method and store the result
+			ArrayList<SearchResultsDiscussions> resultSet = search.searchDiscussions();
+			
+			// Loop through the results and ensure each field of each object is set to a non-null value
+			for (int i = 0; i < resultSet.size(); i++)
+				{
+				// Discussion metadata
+				assertThat(resultSet.get(i).details.discussionID, is(notNullValue()));
+				assertThat(resultSet.get(i).details.discussionName, is(notNullValue()));
+				assertThat(resultSet.get(i).details.submittedDate, is(notNullValue()));
+				assertThat(resultSet.get(i).details.discussionURL, is(notNullValue()));
+				assertThat(resultSet.get(i).details.numberOfComments, is(notNullValue()));
+				
+				// User details
+				assertThat(resultSet.get(i).user.userID, is(notNullValue()));
+				assertThat(resultSet.get(i).user.userName, is(notNullValue()));
+				assertThat(resultSet.get(i).user.userURL, is(notNullValue()));
+				}
+			}
+		
+		catch (IllegalArgumentException | IOException | IllegalStateException | URISyntaxException e)
+			{
+			fail(e.getMessage());
+			}	
+		}
 	}
